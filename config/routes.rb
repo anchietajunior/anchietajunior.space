6e5232528e236b36c9699634fbb8b1b71fd0a34e
Anchieta Júnior <santosjr87@gmail.com>
2019-03-08 23:51:08 -0300

    Add posts and fix title

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  namespace :dashboard do
+    resources :posts
+  end
+  
   get 'pages/index'
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
