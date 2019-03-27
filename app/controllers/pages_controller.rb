bc82d57ad915b60907cb15a6ecbe33c4d88d838b
Anchieta Júnior <santosjr87@gmail.com>
2019-03-27 18:13:49 -0300

    Add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,6 @@ class PagesController < ApplicationController
   def index
     @posts = PostDecorator.decorate_collection(Post.home)
   end
+
+  def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
   # SESSIONS
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login',  to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   # PAGES
   get 'pages/index'
+  get 'pages/about'
   root 'pages#index'
 
   # POSTS
