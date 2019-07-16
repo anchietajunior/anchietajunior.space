dc2ce07c231833be6630b195b633fb5dbdb56f98
Anchieta Júnior <santosjr87@gmail.com>
2019-07-16 11:18:03 -0300

    Add webpack configurations

---
+++ app/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require("webpack");
+ 
+module.exports = {
+  context: __dirname + "/app/assets/javascripts",
+ 
+  entry: {
+    application: "./application.js",
+  },
+ 
+  output: {
+    path: __dirname + "/public",
+    filename: "javascripts/[name].js",
+  },
+ 
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['env']
+        }
+      },
+    ]
+  },
+ 
+};
