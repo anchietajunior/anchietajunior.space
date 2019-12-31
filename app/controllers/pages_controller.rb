c804311066dc370c70ec3da5b46378d30b903fbc
anchietajunior <santosjr87@gmail.com>
2019-12-31 09:55:33 -0300

    Update page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @posts = Post.all.order('created_at DESC').last(5)
+    @posts = Post.all.order('published_at DESC').last(5)
     @projects = nil
     @books = Book.all
   end
