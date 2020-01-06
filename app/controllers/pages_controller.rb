2ac18d4e493b7e502fd847d3aa8f1f1d35fcb44d
anchietajunior <santosjr87@gmail.com>
2020-01-06 06:11:54 -0300

    Order books

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @posts = Post.all.order('published_at DESC').first(5)
     @projects = nil
-    @books = Book.all
+    @books = Book.all.order('created_at DESC')
   end
 
   def about;end
